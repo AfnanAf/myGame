7fec435017ec8752b23aa3666b39686e4573dbff
afnanaaf <afnanalfulaij@gmail.com>
2020-12-27 21:32:41 +0300

    add failurese action ,but not finished yet

--- js/main.js
+++ js/main.js
@@ -23,24 +23,79 @@ $(document).ready(function(){
      }
  
     
- 
+     var failurse = 0;
      $(".letters-container div").click(function(){
         
-        
-         var id = $(this).attr('id');
-         for(letter in secretWordLettersList){
-             if(secretWordLettersList[letter] === id){
-                 $("."+letter+"").text(secretWordLettersList[letter]);
+        var id = $(this).attr('id');
+
+      for(let i=0 ; i<secretWordLettersList.length;i++){
+             if(secretWordLettersList[i] === id){
+                 $("."+letter+"").text(secretWordLettersList[i]);
                  
              }else{
-                 
-                //  c.css("color","red");
-             }
-         }
-         
-     })
- 
-})
+                failurse++;
+                switch(failurse){
+                    case 1: $(".svg-elements-container").append("<rect id=svg_5> />");
+                    $("#svg_5").attr("y",336.46922);
+                    $("#svg_5").attr("x",17.10845);
+
+                    $(".svg-elements-container").append("<rect id=svg_6> />");
+                    $("#svg_6").attr("y",87.46923);
+                    $("#svg_6").attr("x",64.10845);
+
+                    $(".svg-elements-container").append("<rect id=svg_7> />");
+                    $("#svg_7").attr("y",157.46923);
+                    $("#svg_7").attr("x",69.10845);
+
+                    $(".svg-elements-container").append("<rect id=svg_8> />");
+                    $("#svg_8").attr("y",85.46923);
+                    $("#svg_8").attr("x",65.10845);
+
+                    $(".svg-elements-container").append("<rect id=svg_9> />");
+                    $("#svg_9").attr("y",86.46923);
+                    $("#svg_9").attr("x",159.10845);
+                    break;
+                    case 2: 
+                    $(".svg-elements-container").append("<ellipse id=svg_10> />");
+                    $("#svg_10").attr("ry",28.5);
+                    $("#svg_10").attr("rx",29);
+                    $("#svg_10").attr("cy",144.96923);
+                    $("#svg_10").attr("cx",162.10845);
+                    break;
+                    case 3: 
+                    $(".svg-elements-container").append("<rect id=svg_12> />");
+                    $("#svg_12").attr("y",174.46923);
+                    $("#svg_12").attr("x",162.10845);
+                    break;
+                    case 4: 
+                    $(".svg-elements-container").append("<rect id=svg_13> />");
+                    $("#svg_13").attr("y",217.55514);
+                    $("#svg_13").attr("x",158.17616);
+
+                    break;
+                    case 5: 
+                    $(".svg-elements-container").append("<rect id=svg_14> />");
+                    $("#svg_14").attr("y",218.51247);
+                    $("#svg_14").attr("x",121.81482);
+                    break;
+                    case 6: 
+                    $(".svg-elements-container").append("<rect id=svg_15> />");
+                    $("#svg_15").attr("y",270.53004);
+                    $("#svg_15").attr("x",156.65484);
+                    break;
+                    case 7:
+                        $(".svg-elements-container").append("<rect id=svg_16> />");
+                        $("#svg_16").attr("y",271.62465);
+                        $("#svg_16").attr("x",115.30346);    
+                    break;
+                   
+                }
+                console.log(secretWordLettersList[i]);
+            }
+             
+            
+            }
+        });
     
 
 
@@ -48,4 +103,5 @@ $(document).ready(function(){
 
 
 
-
+    
+    });
