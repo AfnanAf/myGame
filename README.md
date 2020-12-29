cacf4644bdc3d593eef9e4b7c6f0b0643bdae6e7
afnanaaf <afnanalfulaij@gmail.com>
2020-12-29 17:34:37 +0300

    The Game is Ready!

--- README.md
+++ README.md
@@ -4,14 +4,40 @@
 ## Wireframes:
 ![Wireframe](/images/wireframe.jpg)
 ## User stories:
-1. As a player I want to be able to start the game when I am ready so that I can relize that the game is started.
-2. As a player I want to be able to end the game when I want so that I can return to the main game page.
-3. As a playe I want to be able to start a new game so that I can start a new game with a new word.
+1. As a Player I want to be able to start the game when I am ready so that I can relize that the game is started.
+2. As a Player I want to be able to end the game when I want so that I can return to the main game page.
+3. As a Player I want to be able to start a new game so that I can start a new game with a new word.
+4. As a Player I want to be able to see a win/fail message so that I know if I won or lost the game.
 <!--Document your planning and tell a story about your development process and problem-solving strategy.-->
+## Development process and problem-solving strategy:
+Firstly, I designed my Wireframes and listed all user stories. Then, I startd constructing the pages' structure using HTML. After that, I begain to design the pages using CSS. Lastly, I begon with adding the logic of the game using JavaScript. Console.log and inspector were very helpful tools in debuging and solving a lot of problems.
 
 <!--List unsolved problems which would be fixed in future iterations.-->
+## Future iterations:
+The game is working good, there is no unsolved problems, but more features could be added in the future iterations like:
+1. Adding more words lists.
+2. Adding Hints.
 ## How to win?
 The player should guess the letters of the secret word correctly before the man is hanged on!
 
 <!--Describe how some of your favorite functions work-->
+## Favorite functions:
+winCheck() function, it checks weather the Player is win or lose the game by cheking if the number of correctly guessed letters is equal to the secret word length and the number of wrongly guessed letters less than 7; the Player win the game and wining message will appear. Otherwise the Player will lose and losing message will appear.
+```javascript
+  var option;
+
+        if (failurse < 7 && $(".secret-word-container").children("div:not(:empty)").length == secretWordLength) {
+            option = alert("Congratulations " + window.location.search.substring(12) + " , You won!");
+            if (option == true) {
+                location.reload();
+
+            }
+        } else if (failurse >= 7 && $(".secret-word-container").children("div:not(:empty)").length <= secretWordLength) {
+            console.log(failurse);
+            option = alert("Unfortunately " + window.location.search.substring(12) + " , You lost :( ");
+            if (option == true) {
+                location.reload();
+            }
+        }
+```
 
--- js/main.js
+++ js/main.js
@@ -1,166 +1,165 @@
-    var foodList = ['APPLE', 'ORANGE', 'BANANA', 'STRAWBERRY', 'POMEGRANATE', 'MANGO', 'GRAPE','PINEAPPLE'];
+var foodList = ['APPLE', 'ORANGE', 'BANANA', 'STRAWBERRY', 'POMEGRANATE', 'MANGO', 'GRAPE', 'PINEAPPLE','BLUEBERRY','AVOCADO','BLACKBERRY','CHERRY','GRAPEFRUIT','KIWI','PAPAYA','PEACH','WATERMELON'];
 
-    var secretWordIndex = Math.floor(Math.random()*foodList.length-1)+0;
-    console.log("index "+secretWordIndex);//1
+var secretWordIndex = Math.floor(Math.random() * foodList.length - 1) + 0;
+console.log("index " + secretWordIndex);//1
 
-    var secretWord = foodList[secretWordIndex];
-    console.log("secretWord "+secretWord);//ORANGE
+var secretWord = foodList[secretWordIndex];
+console.log("secretWord " + secretWord);//ORANGE
 
-    var secretWordLettersList = secretWord.split("");//[O,R,A,N,G,E]
-    console.log("secretWordLettersList "+secretWordLettersList);
+var secretWordLettersList = secretWord.split("");//[O,R,A,N,G,E]
+console.log("secretWordLettersList " + secretWordLettersList);
 
-    var secretWordLength = secretWordLettersList.length;
-    console.log("length "+secretWordLength);//6
+var secretWordLength = secretWordLettersList.length;
+console.log("length " + secretWordLength);//6
 
-$(document).ready(function(){
+$(document).ready(function () {
     var failurse = 0;
 
-    for(let i=0 ; i<secretWordLength ; i++){
-        var letter =  $(".secret-word-container").append("<div class='secret-letter "+i+"'></div>");
+    //appending num of divs depending on how many letters in the secret word
+    for (let i = 0; i < secretWordLength; i++) {
+        $(".secret-word-container").append("<div class='secret-letter " + i + "'></div>");
         $(".secret-letter").addClass("secret-letter");
-   }
- 
-    
- 
- var id=0;
-     $(".letters-container div").on('click',function(){
-       
-        id= $(this).attr('id');
-  
-        
-            
-                if(secretWordLettersList.includes(id,0)){
-               
-                 var arrOfIndexes = getAllIndexes(secretWordLettersList,id);
-                 for(let i=0; i<arrOfIndexes.length;i++){
-                $("."+arrOfIndexes[i]+"").text(id);
-                
-                
-                }
-                winCheck();
-            
-   
-           }else{
-               failurse++;
-              
+    }
 
-               switch(failurse){
-                   case 1: $(".svg-elements-container").append("<rect id=svg_5> />");
-                   $("#svg_5").attr("y",336.46922);
-                   $("#svg_5").attr("x",17.10845);
-   
-                   $(".svg-elements-container").append("<rect id=svg_6> />");
-                   $("#svg_6").attr("y",87.46923);
-                   $("#svg_6").attr("x",64.10845);
-   
-                   $(".svg-elements-container").append("<rect id=svg_7> />");
-                   $("#svg_7").attr("y",157.46923);
-                   $("#svg_7").attr("x",69.10845);
-   
-                   $(".svg-elements-container").append("<rect id=svg_8> />");
-                   $("#svg_8").attr("y",85.46923);
-                   $("#svg_8").attr("x",65.10845);
-   
-                   $(".svg-elements-container").append("<rect id=svg_9> />");
-                   $("#svg_9").attr("y",86.46923);
-                   $("#svg_9").attr("x",159.10845);
-                   break;
-                   case 2: 
-                   $(".svg-elements-container").append("<ellipse id=svg_10> />");
-                   $("#svg_10").attr("ry",28.5);
-                   $("#svg_10").attr("rx",29);
-                   $("#svg_10").attr("cy",144.96923);
-                   $("#svg_10").attr("cx",162.10845);
-                   break;
-                   case 3: 
-                   $(".svg-elements-container").append("<rect id=svg_12> />");
-                   $("#svg_12").attr("y",174.46923);
-                   $("#svg_12").attr("x",162.10845);
-                   break;
-                   case 4: 
-                   $(".svg-elements-container").append("<rect id=svg_13> />");
-                   $("#svg_13").attr("y",217.55514);
-                   $("#svg_13").attr("x",158.17616);
-                   $("#svg_13").attr("transform","rotate(38.30325698852539 184.62823486328128,218.81422424316406)");
 
-   
-                   break;
-                   case 5: 
-                   $(".svg-elements-container").append("<rect id=svg_14> />");
-                   $("#svg_14").attr("y",218.51247);
-                   $("#svg_14").attr("x",121.81482);
-                   $("#svg_14").attr("transform","rotate(133.08865356445312 146.44004821777344,219.75680541992188)");
-                   
-                   break;
-                   case 6: 
-                   $(".svg-elements-container").append("<rect id=svg_15> />");
-                   $("#svg_15").attr("y",270.53004);
-                   $("#svg_15").attr("x",156.65484);
-                   $("#svg_15").attr("transform","rotate(45.65589904785156 186.20304870605472,271.9692077636719)");
 
-                   break;
-                   case 7:
-                       $(".svg-elements-container").append("<rect id=svg_16> />");
-                       $("#svg_16").attr("y",271.62465);
-                       $("#svg_16").attr("x",115.30346);  
-                       $("#svg_16").attr("transform","rotate(128.3930206298828 144.85168457031253,273.06384277343744)");
-  
-                   break;
-                   dafaul:
-                   break;
-                  
-               }//switch
-               
-           }//else 
-           winCheck();
-        
-      
-     });//click func
-     
-    //  winCheck();
- function getAllIndexes(arr, val) {
+    var pressedLetter = 0;// the letter witch the Player has pressed
+    $(".letters-container div").on('click', function () {
+
+        pressedLetter = $(this).attr('id');
+
+        if (secretWordLettersList.includes(pressedLetter, 0)) {
+
+            var arrOfIndexes = getAllIndexes(secretWordLettersList, pressedLetter);//to get all the indexes of the pressedLetter as a list
+            for (let i = 0; i < arrOfIndexes.length; i++) {
+                $("." + arrOfIndexes[i] + "").text(pressedLetter);
+            }
+            winCheck();
+            $(this).css("color","green");
+        } else {
+            failurse++;
+            $(this).css("color","red");
+
+            switch (failurse) {
+                case 1: $(".svg-elements-container").append("<rect id=svg_5> />");
+                    $("#svg_5").attr("y", 336.46922);
+                    $("#svg_5").attr("x", 17.10845);
+
+                    $(".svg-elements-container").append("<rect id=svg_6> />");
+                    $("#svg_6").attr("y", 87.46923);
+                    $("#svg_6").attr("x", 64.10845);
+
+                    $(".svg-elements-container").append("<rect id=svg_7> />");
+                    $("#svg_7").attr("y", 157.46923);
+                    $("#svg_7").attr("x", 69.10845);
+
+                    $(".svg-elements-container").append("<rect id=svg_8> />");
+                    $("#svg_8").attr("y", 85.46923);
+                    $("#svg_8").attr("x", 65.10845);
+
+                    $(".svg-elements-container").append("<rect id=svg_9> />");
+                    $("#svg_9").attr("y", 86.46923);
+                    $("#svg_9").attr("x", 159.10845);
+                    break;
+                case 2:
+                    $(".svg-elements-container").append("<ellipse id=svg_10> />");
+                    $("#svg_10").attr("ry", 28.5);
+                    $("#svg_10").attr("rx", 29);
+                    $("#svg_10").attr("cy", 144.96923);
+                    $("#svg_10").attr("cx", 162.10845);
+                    break;
+                case 3:
+                    $(".svg-elements-container").append("<rect id=svg_12> />");
+                    $("#svg_12").attr("y", 174.46923);
+                    $("#svg_12").attr("x", 162.10845);
+                    break;
+                case 4:
+                    $(".svg-elements-container").append("<rect id=svg_13> />");
+                    $("#svg_13").attr("y", 217.55514);
+                    $("#svg_13").attr("x", 158.17616);
+                    $("#svg_13").attr("transform", "rotate(38.30325698852539 184.62823486328128,218.81422424316406)");
+
+                    break;
+                case 5:
+                    $(".svg-elements-container").append("<rect id=svg_14> />");
+                    $("#svg_14").attr("y", 218.51247);
+                    $("#svg_14").attr("x", 121.81482);
+                    $("#svg_14").attr("transform", "rotate(133.08865356445312 146.44004821777344,219.75680541992188)");
+
+                    break;
+                case 6:
+                    $(".svg-elements-container").append("<rect id=svg_15> />");
+                    $("#svg_15").attr("y", 270.53004);
+                    $("#svg_15").attr("x", 156.65484);
+                    $("#svg_15").attr("transform", "rotate(45.65589904785156 186.20304870605472,271.9692077636719)");
+
+                    break;
+                case 7:
+                    $(".svg-elements-container").append("<rect id=svg_16> />");
+                    $("#svg_16").attr("y", 271.62465);
+                    $("#svg_16").attr("x", 115.30346);
+                    $("#svg_16").attr("transform", "rotate(128.3930206298828 144.85168457031253,273.06384277343744)");
+
+                    break;
+                    dafaul:
+                    break;
+            }//switch
+        }//else 
+        winCheck();
+    });//click func
+
+    $(".letters-container div").hover(function () {
+        $(this).css("border", "3px solid gray");
+}, function(){
+        $(this).css("border", "3px solid black");
+});
+
+    function getAllIndexes(arr, val) {
         var indexes = [], i;
-        for(i = 0; i < arr.length; i++)
+        for (i = 0; i < arr.length; i++)
             if (arr[i] === val)
                 indexes.push(i);
         return indexes;
     }
 
 
-$("#new-link").click(function(){
-    
-    var okOption = confirm("New Game?");
-    if (okOption == true) {
-     location.reload();
-    
-    } 
-})
+    $("#new-link").click(function () {
 
-$("#home-link").click(function(){
-    
-    var okOption = confirm("End Game?");
-    if (okOption == true) {
-        window.location.href = "/html/home.html";
-    } 
-})
-function winCheck(){
-  
-var option;
+        var okOption = confirm("New Game?");
+        if (okOption == true) {
+            location.reload();
 
-  if(failurse<7 && $(".secret-word-container").children("div:not(:empty)").length==secretWordLength){
-    option = alert("Congratulations "+window.location.search.substring(12)+" , You won!");
+        }
+    })
+
+    $("#home-link").click(function () {
+
+        var okOption = confirm("End Game?");
+        if (okOption == true) {
+            window.location.href = "/html/home.html";
+        }
+    })
+    function winCheck() {
+
+        var option;
+
+        if (failurse < 7 && $(".secret-word-container").children("div:not(:empty)").length == secretWordLength) {
+            var winAudio = new Audio('/audio/Ta Da-SoundBible.com-1884170640.mp3');
+            winAudio.play();
+            option = alert("Congratulations " + window.location.search.substring(12) + " , You won!");
             if (option == true) {
                 location.reload();
-                
-               } 
-  }else if(failurse>=7 && $(".secret-word-container").children("div:not(:empty)").length<=secretWordLength){
-    console.log(failurse);
-    option = alert("Unfortunately "+window.location.search.substring(12)+" , You lost :( ");
-    if (option == true) {
-        location.reload();
-       } 
-  }
- }
 
-    
-    });//ready func
+            }
+           
+        } else if (failurse >= 7 && $(".secret-word-container").children("div:not(:empty)").length <= secretWordLength) {
+            var failAudio = new Audio('/audio/Sad_Trombone-Joe_Lamb-665429450.mp3');
+            failAudio.play();
+            option = alert("Unfortunately " + window.location.search.substring(12) + " , You lost :( "+"\n The word is "+secretWord);
+            if (option == true) {
+                location.reload();
+            }
+        }
+    }
+
+});//ready func
